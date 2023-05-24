544fbd2b93a00f397e1183a98f5b9cd2ab381b75
Marc Contreras Massegu <marc.chuki3@gmail.com>
2023-05-24 10:10:21 +0200

    swapped from Orianna to R4J

--- src/main/java/com/vas/metamindslol/MetamindslolApplication.java
+++ src/main/java/com/vas/metamindslol/MetamindslolApplication.java
@@ -1,32 +1,54 @@
 package com.vas.metamindslol;
 
+import no.stelar7.api.r4j.basic.APICredentials;
+import no.stelar7.api.r4j.basic.constants.api.regions.LeagueShard;
+import no.stelar7.api.r4j.impl.R4J;
+import no.stelar7.api.r4j.impl.lol.builders.summoner.SummonerBuilder;
+import no.stelar7.api.r4j.impl.lol.raw.ImageAPI;
+import no.stelar7.api.r4j.pojo.lol.match.v5.LOLMatch;
+import no.stelar7.api.r4j.pojo.lol.match.v5.MatchParticipant;
+import no.stelar7.api.r4j.pojo.lol.match.v5.MatchPerks;
+import no.stelar7.api.r4j.pojo.lol.staticdata.champion.StaticChampion;
+import no.stelar7.api.r4j.pojo.lol.summoner.Summoner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.merakianalytics.orianna.Orianna;
-import com.merakianalytics.orianna.types.common.Queue;
-import com.merakianalytics.orianna.types.common.Region;
-import com.merakianalytics.orianna.types.core.league.League;
-import com.merakianalytics.orianna.types.core.staticdata.Champion;
-import com.merakianalytics.orianna.types.core.staticdata.Champions;
-import com.merakianalytics.orianna.types.core.summoner.Summoner;
+
+import java.util.List;
+import java.util.Map;
+
 
 @SpringBootApplication
 public class MetamindslolApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MetamindslolApplication.class, args);
-		Orianna.setRiotAPIKey("RGAPI-a5a42536-e6fc-425d-a967-e4e59e7a2012");
-		Orianna.setDefaultRegion(Region.NORTH_AMERICA);
+    private static final String apiKey = "RGAPI-9f23e9dd-c474-4cbc-854c-7ef1ad4c2acc";
 
-		Summoner summoner = Orianna.summonerNamed("FatalElement").get();
-		System.out.println(summoner.getName() + " is level " + summoner.getLevel() + " on the " + summoner.getRegion() + " server.");
+    public static void main(String[] args) {
+        SpringApplication.run(MetamindslolApplication.class, args);
+        APICredentials creds = new APICredentials(apiKey);
+        R4J api = new R4J(creds);
+        String user = "simply me";
+        LeagueShard region = LeagueShard.EUW1;
 
-		Champions champions = Orianna.getChampions();
-		Champion randomChampion = champions.get((int)(Math.random() * champions.size()));
-		System.out.println("He enjoys playing champions such as " + randomChampion.getName());
+        Summoner summoner = new SummonerBuilder().withPlatform(region).withName(user).get();
+        Map<Integer, StaticChampion> champData = api.getDDragonAPI().getChampions();
+        //pfp
+        String pfp = ImageAPI.getInstance().getProfileIcon(region, user);
+        //name and lv
+        int level = summoner.getSummonerLevel();
+        String name = summoner.getName();
+        //most recent game
+        List<String> matches = summoner.getLeagueGames().get();
+        LOLMatch match = LOLMatch.get(region, matches.get(0));
+        MatchParticipant self = match.getParticipants().stream().filter(p -> p.getPuuid().equals(summoner.getPUUID())).findFirst().get();
+        StaticChampion champion = champData.get(self.getChampionId());
+        MatchPerks summs = self.getPerks();
+        boolean won = self.didWin();
 
-		League challengerLeague = Orianna.challengerLeagueInQueue(Queue.RANKED_SOLO).get();
-		Summoner bestNA = challengerLeague.get(0).getSummoner();
-		System.out.println("He's not as good as " + bestNA.getName() + " at League, but probably a better Java programmer!");	}
-
+        System.out.println("Profile icon: " + pfp);
+        System.out.println(name + ", Level " + level);
+        System.out.println();
+        System.out.format(name + " %s their most recent game.", won ? "won" : "lost");
+        System.out.println();
+        System.out.println("They were playing " + self.getChampionSelectLane() + " " + champion.getName());
+    }
 }
