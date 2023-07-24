bb12bd7ccafcd354206cfd43b506d87ebef1533f
Marc Contreras Massegu <marc.chuki3@gmail.com>
2023-07-24 20:29:37 +0200

    now only will load games with 10 participants, yes I'm looking at you arena_cherry

--- src/main/java/com/vas/metamindslol/matchV5/MatchService.java
+++ src/main/java/com/vas/metamindslol/matchV5/MatchService.java
@@ -2,7 +2,6 @@ package com.vas.metamindslol.matchV5;
 
 import com.vas.metamindslol.ModelMapperConfig;
 import com.vas.metamindslol.R4JInstance;
-import com.vas.metamindslol.Timeline.TimelineRepository;
 import com.vas.metamindslol.Timeline.TimelineService;
 import com.vas.metamindslol.exception.NotFoundException;
 import no.stelar7.api.r4j.basic.constants.api.regions.LeagueShard;
@@ -30,6 +29,10 @@ public class MatchService {
 
     @Autowired
     TimelineService timelineService;
+
+    public final String SPECIAL_GAME_MODE = "EVENT GAME MODE";
+
+
 //LOADING RECENT GAME INTO DB
 
     /**
@@ -47,7 +50,10 @@ public class MatchService {
             summoner = R4JInstance.loLAPI.getSummonerAPI().getSummonerByName(opShard.get(), summonerName);
             List<String> matches = summoner.getLeagueGames().get();
             match = LOLMatch.get(opShard.get(), matches.get(0));
-            matchDD = findOrSaveMatch(match);
+            if (match.getParticipants().size() == 10)
+                matchDD = findOrSaveMatch(match);
+            else
+                return SPECIAL_GAME_MODE;
         }
         return gson.toJson(Objects.requireNonNullElse(matchDD, new NotFoundException().getMessage()));
 
@@ -58,7 +64,7 @@ public class MatchService {
      * @param summonerName
      * @return The most recent match of the given summoner if it exists
      */
-    public String loadMatchBySummonerName(String region, String summonerName, Integer count,Integer gameNumber) {
+    public String loadMatchBySummonerName(String region, String summonerName, Integer count, Integer gameNumber) {
         if (count == null) {
             count = 20;
             gameNumber = 0;
@@ -70,10 +76,13 @@ public class MatchService {
         if (opShard.isPresent()) {
             summoner = R4JInstance.loLAPI.getSummonerAPI().getSummonerByName(opShard.get(), summonerName);
             //add the start to be able to get games after the 100 first
-            List<String> matches =  new MatchListBuilder().withPlatform(summoner.getPlatform()).withPuuid(summoner.getPUUID()).withCount(count).get();
+            List<String> matches = new MatchListBuilder().withPlatform(summoner.getPlatform()).withPuuid(summoner.getPUUID()).withCount(count).get();
             match = LOLMatch.get(opShard.get(), matches.get(gameNumber));
-            matchDD = findOrSaveMatch(match);
-            timelineService.loadTimelineByGameId(region, matchDD.getGameId());
+            if (match.getParticipants().size() == 10) {
+                matchDD = findOrSaveMatch(match);
+                timelineService.loadTimelineByGameId(region, matchDD.getGameId());
+            } else
+                return SPECIAL_GAME_MODE;
         }
         return gson.toJson(Objects.requireNonNullElse(matchDD, new NotFoundException().getMessage()));
 
@@ -128,30 +137,33 @@ public class MatchService {
 
         //we have atleast a game in the db
         String matchString = getMostRecentMatchBySummonerName(region, summonerName);
-        if (!matchString.equals(gson.toJson(new NotFoundException().getMessage(),String.class))) {
+        if (!matchString.equals(gson.toJson(new NotFoundException().getMessage(), String.class))) {
             match = gson.fromJson(matchString, LOLMatchDD.class);
-            matchId=match.getGameId();
+            matchId = match.getGameId();
         }
 
         List<LOLMatchDD> matchesDB = new ArrayList<>();
-        LOLMatchDD matchTemp=null;
+        LOLMatchDD matchTemp = null;
         int count = 0;
         // deal with the start for now will show only a page which is 100
-        int maxGamesAllowed=100;
+        int maxGamesAllowed = 100;
         boolean found = false;
-
-        while (!found && count<maxGamesAllowed) {
-            matchString= loadMatchBySummonerName(region, summonerName,maxGamesAllowed, count);
-            if (!matchString.equals(new NotFoundException().getMessage())) {
-                matchTemp = gson.fromJson(matchString, LOLMatchDD.class);
-                timelineService.loadTimelineByGameId(region, matchTemp.getGameId());
-                if (matchTemp.getGameId() == matchId)
-                    found = true;
-                matchesDB.add(matchTemp);
-            }else if(count==0)
+        String exc = new NotFoundException().getMessage();
+        String notFound = gson.toJson(exc, String.class);
+        while (!found && count < maxGamesAllowed) {
+            matchString = loadMatchBySummonerName(region, summonerName, maxGamesAllowed, count);
+            if (!matchString.equals(gson.toJson(new NotFoundException().getMessage(), String.class))) {
+                if(!matchString.equals(SPECIAL_GAME_MODE)) {
+                    matchTemp = gson.fromJson(matchString, LOLMatchDD.class);
+                    timelineService.loadTimelineByGameId(region, matchTemp.getGameId());
+                    if (matchTemp.getGameId() == matchId)
+                        found = true;
+                    matchesDB.add(matchTemp);
+                }
+            } else if (count == 0)
                 gson.toJson(new NotFoundException("games"));//return games not found
             else
-                found=true;
+                found = true;
 
             count++;
         }
@@ -190,7 +202,10 @@ public class MatchService {
         Optional<LeagueShard> opShard = LeagueShard.fromString(region);
         if (opShard.isPresent()) {
             match = LOLMatch.get(opShard.get(), matches.get(0));
-            matchDD = findOrSaveMatch(match);
+            if (match.getParticipants().size() == 10)
+                matchDD = findOrSaveMatch(match);
+            else
+                return SPECIAL_GAME_MODE;
         }
         return gson.toJson(Objects.requireNonNullElse(matchDD, new NotFoundException().getMessage()));
 
@@ -209,6 +224,8 @@ public class MatchService {
         Optional<LeagueShard> opShard = LeagueShard.fromString(region);
         if (opShard.isPresent()) {
             match = LOLMatch.get(opShard.get(), matches.get(matchNumber));
+            if (match.getParticipants().size() != 10)
+                return SPECIAL_GAME_MODE;
         }
         return gson.toJson(Objects.requireNonNullElse(match, new NotFoundException().getMessage()));
 
